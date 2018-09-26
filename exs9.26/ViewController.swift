56ec8b52ae71873dd233c40c2fc7a204451d611e
20161104597 <Stereophonic@admindeiMac-38.local>
2018-09-26 11:46:52 +0800

    Initial commit

---
+++ exs9.26/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  exs9.26
+//
+//  Created by $tereophonic on 2018/9/26.
+//  Copyright © 2018年 $tereophonic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    
+    @IBAction func n0(_ sender: Any) {
+    }
+    
+    @IBAction func n1(_ sender: Any) {
+    }
+    
+    @IBAction func n2(_ sender: Any) {
+    }
+    
+    @IBAction func n3(_ sender: Any) {
+    }
+    
+    @IBAction func n4(_ sender: Any) {
+    }
+    
+    @IBAction func n5(_ sender: Any) {
+    }
+    
+    @IBAction func n6(_ sender: Any) {
+    }
+    
+    @IBAction func n7(_ sender: Any) {
+    }
+    
+    @IBAction func n8(_ sender: Any) {
+    }
+    
+    @IBAction func n9(_ sender: Any) {
+    }
+    
+    @IBAction func point(_ sender: Any) {
+    }
+    
+    @IBAction func add(_ sender: Any) {
+    }
+    
+    @IBAction func minus(_ sender: Any) {
+    }
+    
+    @IBAction func multiply(_ sender: Any) {
+    }
+    
+    @IBAction func divide(_ sender: Any) {
+    }
+    @IBAction func equal(_ sender: Any) {
+    }
+    
+}
+
+
+
---
+++ exs9.26Tests/exs9_26Tests.swift
@@ -0,0 +1,34 @@
+//
+//  exs9_26Tests.swift
+//  exs9.26Tests
+//
+//  Created by $tereophonic on 2018/9/26.
+//  Copyright © 2018年 $tereophonic. All rights reserved.
+//
+
+import XCTest
+@testable import exs9_26
+
+class exs9_26Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
